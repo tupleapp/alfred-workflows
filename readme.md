740ad1a50c6f3987026c5e950690c71b569d2d19
Spencer Dixon <spencercdixon@gmail.com>
2019-09-03 13:26:25 -0400

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tuple Alfred Workflows
+
+A collection of [Alfred](https://www.alfredapp.com/) workflows for interacting with [Tuple.app](https://tuple.app/).
---
+++ scripts/online_users.rb
@@ -0,0 +1,75 @@
+# online_users.rb
+#
+# Author: Spencer Dixon <spencer@tuple.app>
+# Copyright: Tuple, LLC 
+
+query = ARGV[0]
+
+# -------
+# Utility 
+# -------
+
+def pbpaste
+  `pbpaste`
+end
+
+def pbcopy(input)
+ str = input.to_s
+ IO.popen('pbcopy', 'w') { |f| f << str }
+ str
+end
+
+def with_sandboxed_clipboard(&block)
+  previous = pbpaste
+  yield block
+  pbcopy(previous)
+end
+
+def regex_fuzzy_search(query, list) 
+  query_reg = /#{query.split('').join('.*?')}/
+  sorted = []
+  list.each do |string|
+    match = query_reg.match string
+    sorted << {string: string, rank: match.to_s.length} if match
+  end
+  sorted.sort_by! {|i| i[:rank] }
+  sorted
+end
+
+# ----
+# Main
+# ----
+
+with_sandboxed_clipboard do
+  # -g to prevent Tuple from coming to foreground. We want Alfred to remain key.
+  system('open -g tuple://online-users')
+
+  online_users = pbpaste
+
+  if online_users.include?(',') 
+    users = online_users.split(',')
+  else
+    users = [online_users]
+  end
+
+  sorted = regex_fuzzy_search(query, users)
+
+  users_string = ""
+  if sorted.count == 0
+    users_string = "<item><title>No online users</title></item>"
+  else
+    sorted.each do |user|
+      users_string << %Q{<item arg="#{user[:string]}"><title>#{user[:string]}</title></item>}
+    end
+  end
+
+  xml = <<EOS
+<xml>
+  <items>
+    #{users_string}
+  </items>
+</xml>
+EOS
+
+  puts xml
+end
